780346abdcd529e253faa9de22d27ebdff9612bb
coolt <tom.cool@tomtom.com>
2021-09-15 09:37:02 +0200

    added comments

--- src/Sudoku.ts
+++ src/Sudoku.ts
@@ -3,13 +3,20 @@ type SeriesIndex = (e: unknown, i: number) => unknown
 
 export class Sudoku {
 
+    /**
+     * Numberrrrr
+     */
     public static readonly MIN_VALUE: number = 0
     public static readonly MAX_VALUE: number = 9
     public static readonly MAX_CELLS: number = 81
 
-    public static createSeries<T>(indicator: number): T
+    /**
+     * Create a Series of N length, filled with an Array of Numbers.
+     * @param argument Either a Number or a Method, both are ways to provide values for a Series
+     */ 
+    public static createSeries<T>(value: number): T
     public static createSeries<T>(fill: SeriesIndex): T
-    public static createSeries<T>(argument?: number | SeriesIndex): T {
+    public static createSeries<T>(argument: number | SeriesIndex): T {
         
         let index: number = 0
         let mapFn: SeriesIndex = () => index
