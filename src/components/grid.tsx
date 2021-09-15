a68053ad586c520c4b803da94d7f4c890ba3c9a4
coolt <tom.cool@tomtom.com>
2021-09-15 21:49:52 +0200

    somehow the merge went wrong

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -2,10 +2,9 @@
 import { Children, FC, useEffect, useCallback, memo } from 'react'
 import { Dispatch, AnyAction } from 'redux'
 import { useDispatch, useSelector } from 'react-redux'
-import useMouseTrap from 'react-hook-mousetrap'
 import * as Styled from '../styles'
-import { Block, Numbers, ResetGameButton } from './'
-import { createGrid, StoreReducer, selectCell, fillCell } from '../redux'
+import { AttachKeyBoardEvents, Block, Numbers, ResetGameButton } from './'
+import { createGrid, StoreReducer, fillCell } from '../redux'
 import { GridMatrix, GridMatrixCoörds, GridMatrixIndex, N, SudokuInput } from '../typings'
 
 interface GridState {
@@ -34,60 +33,13 @@ export const Grid: FC = memo(() => {
         }
     }, [dispatch, selection, selectedValue])
 
-    function moveDown() {
-        if (selection && selection[0] < 8) {
-            dispatch(selectCell([
-                (selection[0] + 1) as GridMatrixIndex, selection[1]
-            ]))
-        }
-    }
-
-    function moveLeft() {
-        if (selection && selection[1] > 0) {
-            dispatch(selectCell([
-                selection[0], (selection[1] - 1) as GridMatrixIndex
-            ]))
-        }
-    }
-
-    function moveRight() {
-        if (selection && selection[1] < 8)  {
-            dispatch(selectCell([
-                selection[0], (selection[1] + 1) as GridMatrixIndex
-            ]))
-        }
-    }
-
-    function moveUp() {
-        if (selection && selection[0] > 0) {
-            dispatch(selectCell([
-                (selection[0] - 1) as GridMatrixIndex, selection[1]
-            ]))
-        }
-    }
-
-    useMouseTrap('1', () => fill(1))
-    useMouseTrap('2', () => fill(2))
-    useMouseTrap('3', () => fill(3))
-    useMouseTrap('4', () => fill(4))
-    useMouseTrap('5', () => fill(5))
-    useMouseTrap('6', () => fill(6))
-    useMouseTrap('7', () => fill(7))
-    useMouseTrap('8', () => fill(8))
-    useMouseTrap('9', () => fill(9))
-
-    useMouseTrap('down', moveDown)
-    useMouseTrap('down', moveDown)
-    useMouseTrap('left', moveLeft)
-    useMouseTrap('right', moveRight)
-    useMouseTrap('up', moveUp)
-
     useEffect(() => { 
         if (!solvedGrid) create() 
     }, [create, solvedGrid])
 
     return (
         <>
+            <AttachKeyBoardEvents selection={selection} numbersInputHandler={fill} />
             <Styled.GridContainer>
                 <ResetGameButton reset={create} />
             </Styled.GridContainer>
