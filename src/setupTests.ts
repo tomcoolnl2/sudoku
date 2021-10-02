66e519735b2f0f1e12940d246da0d0d1152872b4
coolt <tom.cool@tomtom.com>
2021-10-02 20:38:23 +0200

    Adding jest-styled-components fixed the bug generated classnames, and created updated snapshots.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,3 +1,7 @@
+// A set of utilities for testing Styled Components with Jest. 
+// This package improves the snapshot testing experience and 
+// provides a brand new matcher to make expectations on the style rules.
+import 'jest-styled-components'
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
