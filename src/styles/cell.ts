dcb98173b1715eb6e09e10f83709e23550b5370e
coolt <tom.cool@tomtom.com>
2021-10-06 21:57:56 +0200

    highlight duplicate error values

--- src/styles/cell.ts
+++ src/styles/cell.ts
@@ -8,34 +8,44 @@ interface CellContainerProps {
     clue?: boolean
     duplicate?: boolean
     mistake?: boolean
+    mistakeDuplicate?: boolean
 }
 
 export const CellContainer = styled.div<CellContainerProps>`
-    ${({ theme, clue, selected, highlighted, duplicate, mistake }) => css`
-        align-items: center;
-        background-color: ${theme.colors[selected || duplicate ? 'active' : (highlighted ? 'highlighted' : 'primary')]};
-        border: 1px solid ${theme.colors.secondary};
-        cursor: pointer;
-        color: ${mistake ? '#CB4335' : 'inherit'};
-        display: flex;
-        flex-grow: 1;
-        flex-shrink: 0;
-        flex-basis: 0;
-        font-size: 20px;
-        font-weight: ${clue ? 'bold' : 'normal'};
-        height: auto;
-        justify-content: center;
-        transition: ${theme.transition};
-        user-select: none;
+    ${({ theme, clue, selected, highlighted, duplicate, mistake, mistakeDuplicate }) => {
 
-        &::before {
-            padding-top: 100%;
-            content: '';
-            float: left;
-        }
+		let bgColor: keyof typeof theme.colors
+		if (selected || duplicate) bgColor = 'active'
+		else if (highlighted) bgColor = 'highlighted'
+		else if (mistake || mistakeDuplicate) bgColor = 'errorSecundary'
+		else bgColor = 'primary'
 
-        &:hover {
-            background-color: ${theme.colors[selected ? 'active' : 'highlighted']}
-        }
-    `}
+		return css`
+            align-items: center;
+            background-color: ${theme.colors[bgColor]};
+            border: 1px solid ${theme.colors.secondary};
+            cursor: pointer;
+            color: ${mistake || mistakeDuplicate ? theme.colors.errorPrimary : 'inherit'};
+            display: flex;
+            flex-grow: 1;
+            flex-shrink: 0;
+            flex-basis: 0;
+            font-size: 20px;
+            font-weight: ${clue ? 'bold' : 'normal'};
+            height: auto;
+            justify-content: center;
+            transition: ${theme.transition};
+            user-select: none;
+
+            &::before {
+                padding-top: 100%;
+                content: '';
+                float: left;
+            }
+
+            &:hover {
+                background-color: ${theme.colors[selected ? 'active' : 'highlighted']}
+            }
+        `}
+}
 `
